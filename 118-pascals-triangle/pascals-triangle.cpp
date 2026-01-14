16d456cd95dbf0842daa304364ff56dbf8b77139
ANUBHAV SINGH RAJPUT <anubhavsinghrajputhere@gmail.com>
2026-01-14 21:20:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (31.61%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1);
+            
+            // First and last elements are always 1
+            row[0] = row[i] = 1;
+            
+            // Fill middle elements using previous row
+            for (int j = 1; j < i; j++) {
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j];
+            }
+            
+            triangle.push_back(row);
+        }
+        return triangle;
+    }
+};
