b055ae71873ebf288c7a0e88eed2ebe7ac1d241c
ANUBHAV SINGH RAJPUT <anubhavsinghrajputhere@gmail.com>
2026-01-23 19:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (27.28%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int candidate;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+};
