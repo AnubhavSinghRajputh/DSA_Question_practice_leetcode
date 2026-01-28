a15ef555ff08f6f8421e8c769217892ae802e31d
ANUBHAV SINGH RAJPUT <anubhavsinghrajputhere@gmail.com>
2026-01-28 23:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (6.46%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getSumOfSquares(n);
+        }
+        
+        return n == 1;
+    }
+};
