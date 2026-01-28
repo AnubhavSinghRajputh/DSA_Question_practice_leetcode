6786f0bf756234c1b27e8005ca9e596206059528
ANUBHAV SINGH RAJPUT <anubhavsinghrajputhere@gmail.com>
2026-01-28 23:10:20 +0530

    Time: 5 ms (58.35%) | Memory: 13.3 MB (36.99%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int result = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            // If current value < next value, it's subtractive (IV, IX, etc.)
+            if (i + 1 < n && roman[s[i]] < roman[s[i + 1]]) {
+                result += roman[s[i + 1]] - roman[s[i]];
+                i++;  // Skip next character
+            } else {
+                result += roman[s[i]];
+            }
+        }
+        
+        return result;
+    }
+};
